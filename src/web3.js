275b8f7f12a290bf43ed80753ba6e323105f8604
dmitryshlymovich <shdmitry@gmail.com>
2022-05-02 18:50:21 +0300

    ropsten

--- src/web3.js
+++ src/web3.js
@@ -16,20 +16,20 @@ const mnemonic = "mixed exercise gorilla coyote response trap actual wife poem b
 //);
 
 
-export default new Web3(
-  new Web3.providers.HttpProvider(
-    //`http://127.0.0.1:7545`,
-    `http://52.39.220.74:8545`,
-  )
-);
+//export default new Web3(
+//  new Web3.providers.HttpProvider(
+//    //`http://127.0.0.1:7545`,
+//    `http://52.39.220.74:8545`,
+//  )
+//);
 
 const NODE_API_KEY = process.env.INFURA_KEY || process.env.ALCHEMY_KEY;
 const isInfura = !!process.env.INFURA_KEY;
 //const provider =new HDWalletProvider(mnemonic, "wss://rinkeby.infura.io/ws/v3/22061bf4f360438997f0055e5c095cf4");
 //const provider =new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/22061bf4f360438997f0055e5c095cf4");
-//export default new Web3(provider.engine);
+const provider =new HDWalletProvider(mnemonic, "wss://ropsten.infura.io/ws/v3/22061bf4f360438997f0055e5c095cf4");
+export default new Web3(provider.engine);
 
-//export default new Web3(new HDWalletProvider(mnemonic, "wss://rinkeby.infura.io/ws/v3/22061bf4f360438997f0055e5c095cf4"));
 
 
 
